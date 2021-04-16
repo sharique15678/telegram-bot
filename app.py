c5d362ab8c6d72281e4dabbeb1f57170129dc8f8
Md Sharique <sharique15678@gmail.com>
2021-04-16 22:39:53 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,72 @@
+import re
+from flask import Flask, request
+import telegram
+from telebot.credentials import bot_token, bot_user_name, URL
+
+# Here are some credentials
+bot_token = "1624210226:AAHTw0ZfchbSOpAWB_eW37xvuigk07C9lnc"
+bot_user_name = "sharique15678_view_bot"
+URL = "heroku app link"
+
+
+global bot
+global TOKEN
+TOKEN = bot_token
+bot = telegram.Bot(token=TOKEN)
+
+app = Flask(__name__)
+
+
+@app.route('/{}'.format(TOKEN), methods=['POST'])
+def respond():
+    # retrieve the message in JSON and then transform it to Telegram object
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id
+
+    # Telegram understands UTF-8, so encode text for unicode compatibility
+    text = update.message.text.encode('utf-8').decode()
+    # for debugging purposes only
+    print("got text message :", text)
+    # the first time you chat with the bot AKA the welcoming message
+    if text == "/start":
+        # print the welcoming message
+        bot_welcome = """
+        Welcome to my auto view bot.
+        """
+        # send the welcoming message
+        bot.sendMessage(chat_id=chat_id, text=bot_welcome,
+                        reply_to_message_id=msg_id)
+    else:
+        try:
+            bot_welcome = """
+            Welcome to my auto view bot.
+            """
+            # send the welcoming message
+            bot.sendMessage(chat_id=chat_id, text=bot_welcome,
+                            reply_to_message_id=msg_id)
+        except Exception:
+            # if things went wrong
+            bot.sendMessage(
+                chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
+
+    return 'ok'
+
+
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
+
+
+@app.route('/')
+def index():
+    return '.'
+
+
+if __name__ == '__main__':
+    app.run(threaded=True)
